c81c17d70a238da6338ac1da9f95e195080cc74f
JAY RANK <53647573+RankJay@users.noreply.github.com>
2020-07-11 22:44:35 +0530

    Update LandingPage.js

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import Home from './Home';
+import Default from './Default';
 import AboutMePage from "./AboutMePage";
 import SkillsPage from "./SkillsPage";
 import AchievementsPage from "./AchievementsPage";
@@ -19,12 +19,43 @@ import { NavLink, Link, Redirect } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 class LandingPage extends React.Component {
+  componentDidMount() {
+    document.title = 'Rank Jay';
+    document.oncontextmenu = new Function("return false;")
+    document.addEventListener('contextmenu', e => e.preventDefault());
+    document.onkeydown = function (e) {
+      if (e.keyCode == 123) {
+        alert("This function has been disabled to prevent you from stealing my code!");
+        return false;
+      }
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        alert("This function has been disabled to prevent you from stealing my code!");
+        return false;
+      }
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        alert("This function has been disabled to prevent you from stealing my code!");
+        return false;
+      }
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        alert("This function has been disabled to prevent you from stealing my code!");
+        return false;
+      }
+      if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        alert("This function has been disabled to prevent you from stealing my code!");
+        return false;
+      }
+      if (e.ctrlKey && e.keyCode == 'C'.charCodeAt(0)) {
+        alert("Just try being original for once!");
+        return false;
+      }
+    }
+  }
   render() {
-  return (
+    return (
       <div className="LandingPage">
-        <div className="AboutMeRow">
-          <Router>
-            <nav>
+        <Router>
+          <nav>
+            <div className="AboutMeRow">
               <div className="AboutMe"><Lnk to="/AboutMePage">About Me</Lnk></div>
               <div className="Skills"><Lnk to="/SkillsPage">Skills</Lnk></div>
               <div className="Achievements"><Lnk to="/AchievementsPage">Achievements</Lnk></div>
@@ -32,27 +63,25 @@ class LandingPage extends React.Component {
               <div className="Projects"><Lnk to="/ProjectsPage">Projects</Lnk></div>
               <div className="Gallery"><Lnk to="/GalleryPage">Gallery</Lnk></div>
               <div className="Contact"><Lnk to="/ContactPage">Contact</Lnk></div>
-            </nav>
-          </Router>
-        </div>
-        <React.Fragment>
-          <Router>
+            </div>
+          </nav>
+          <React.Fragment>
             <Switch>
               <Route path='/AboutMePage' component={AboutMePage} />
               <Route path='/SkillsPage' component={SkillsPage} />
-              <Route path='/AchievementsPage' component={AchievementsPage} />
-              <Route path='/ResearchPage' component={ResearchPage} />
-              <Route path='/ProjectsPage' component={ProjectsPage} />
-              <Route path='/GalleryPage' component={GalleryPage} />
-              <Route path='/ContactPage' component={ContactPage} />
+              <Route path='/AchievementsPage' component={Default} />
+              <Route path='/ResearchPage' component={Default} />
+              <Route path='/ProjectsPage' component={Default} />
+              <Route path='/GalleryPage' component={Default} />
+              <Route path='/ContactPage' component={Default} />
               <Route path='/' component={Home} />
             </Switch>
             <Redirect to="/Home"></Redirect>
-          </Router>
-        </React.Fragment>
+          </React.Fragment>
+        </Router>
       </div>
-  );
-}
+    );
+  }
 }
 
 const Image = styled.img`
@@ -60,6 +89,14 @@ const Image = styled.img`
   align: center;
   height: 80%;
 `;
+
+const Image1 = styled.img`
+margin: auto;
+margin-top: 15%; 
+width: 95%;
+height: 95%;
+`;
+
 const Lnk = styled(Link)`
     text-decoration: none;
     color: #ffffff;
